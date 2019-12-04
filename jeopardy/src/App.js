c74db22b6108dbb20eb6f5e4f81a5cf418485391
JarredStanford <stanj89@gmail.com>
2019-12-03 21:20:59 -0500

    functioning

--- jeopardy/src/App.js
+++ jeopardy/src/App.js
@@ -1,41 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
 import './App.css';
+import styled from 'styled-components'
 
 import useForm from './utils/useForm'
 
 function App() {
 
   const [question, setQuestion] = useState()
-  const [category, setCategory] = useState()
   const [total, setTotal] = useState(0)
-  const [selected, setSelected] = useState()
 
   const { values, handleChange, handleSubmit } = useForm(checkAnswer);
 
   useEffect(() => {
     const getQuestion = async () => {
-      const result = await axios.get('http://localhost:8000/answers/episodes')
+      const result = await axios.get('http://localhost:8000/answers/random')
+      setQuestion(result.data)
     }
     getQuestion()
 
     return setQuestion()
   }, [])
 
-  /*const nextQuestion = async () => {
+  const nextQuestion = async () => {
     const result = await axios.get('http://localhost:8000/answers/random')
     setQuestion(result.data)
-  }*/
+  }
 
   function checkAnswer() {
-    console.log()
-    if (selected.answer.toLowerCase().includes(values.answer.toLowerCase())) {
+    if (question.answer.toLowerCase().includes(values.answer.toLowerCase())) {
       console.log("Correct!")
-      setTotal(total => total += Number(selected.value.replace(/[$,]+/g, '')))
+      setTotal(total => total += Number(question.value.replace(/[$,]+/g, '')))
     } else {
       console.log("Incorrect!" + question.answer)
-      setTotal(total => total -= Number(selected.value.replace(/[$,]+/g, '')))
+      setTotal(total => total -= Number(question.value.replace(/[$,]+/g, '')))
     }
+    nextQuestion()
   }
 
   if (question === undefined) return <div>Loading...</div>
@@ -43,17 +43,13 @@ function App() {
   return (
     <>
       <div style={{ marginBottom: '2%' }}>
-        {category}
+        {question.category}
       </div>
       <div>
-        <div onClick={() => setSelected(question[0])}>{question[0].value}</div>
-        <div onClick={() => setSelected(question[1])}>{question[1].value}</div>
-        <div onClick={() => setSelected(question[2])}>{question[2].value}</div>
-        <div onClick={() => setSelected(question[3])}>{question[3].value}</div>
-        <div onClick={() => setSelected(question[4])}>{question[4].value}</div>
+        {question.question}
       </div>
       <div>
-        {selected && selected.question}
+        {question.value}
       </div>
       <form onSubmit={handleSubmit}>
         <input
